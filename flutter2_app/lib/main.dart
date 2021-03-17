209caec580a68eb3be49f82d91489e29f7db36f8
GhifarZy <login0557@gmail.com>
2021-03-18 00:40:48 +0700

    membuat_navigasi

--- flutter2_app/lib/main.dart
+++ flutter2_app/lib/main.dart
@@ -64,18 +64,49 @@ class _RandomWordsState extends State<RandomWords> {
     );
   }
   // #enddocregion _buildRow
-
-  // #docregion RWS-build
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Startup Name Generator'),
+        actions: [
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+        ],
       ),
       body: _buildSuggestions(),
     );
   }
-// #enddocregion RWS-build
+  // #docregion RWS-build
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        // NEW lines from here...
+        builder: (BuildContext context) {
+          final tiles = _saved.map(
+                (WordPair pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final divided = ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+          ).toList();
+
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );
+        }, //...to here.
+      ),
+    );
+  }
 // #docregion RWS-var
 }
 // #enddocregion RWS-var
